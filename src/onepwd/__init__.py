77cf0680665b22519a0880246b2d3823dec981e1
Sam P (Spury) <s.pur@outlook.com>
2026-06-30 17:52:35 +0100

    Update version

--- src/onepwd/__init__.py
+++ src/onepwd/__init__.py
@@ -3,5 +3,5 @@
 from onepwd.client import OnePasswordClient
 from onepwd.exceptions import OnePasswordError
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = ["OnePasswordClient", "OnePasswordError", "__version__"]
